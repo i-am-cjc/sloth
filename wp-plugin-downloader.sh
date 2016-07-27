fd0395e0968622a8266e7a45febb30b2ed26bf26
Carl Clegg <carl@cjc.im>
2016-07-27 08:04:07 +0100

    Now added a list of plugins that we have already seen to stop redownloading the same things a couple of days later

--- wp-plugin-downloader.sh
+++ wp-plugin-downloader.sh
@@ -11,26 +11,30 @@ fi
 
 for URL in $(cat .tmp/list.html | grep "plugin-icon" | cut -d "\"" -f 2); do
 	PLUGIN=$(echo $URL | cut -d "/" -f 5)
-	echo "[+] Downloading $PLUGIN..."
-	wget -q $URL -O .tmp/$PLUGIN.html
-	DL=$(cat .tmp/$PLUGIN.html | grep "downloadUrl" | cut -d "'" -f 4)
-	wget -q $DL -O files/$PLUGIN.zip
+    if grep -q $PLUGIN .list; then
+        echo "[!] Already seen $PLUGIN not downloading"
+    else
+        echo "[+] Downloading $PLUGIN..."
+        wget -q $URL -O .tmp/$PLUGIN.html
+        DL=$(cat .tmp/$PLUGIN.html | grep "downloadUrl" | cut -d "'" -f 4)
+        wget -q $DL -O files/$PLUGIN.zip
+        echo $PLUGIN >> .list
+    fi
 done
 
 cd files
 
+echo "[+] Extracting any files"
 for FILE in $(ls); do
-	echo "[+] Extracting $FILE"
 	unzip $FILE > /dev/null 2>&1
 done
 cd ..
 
 echo "[+] Cleaning up"
-rm -rf .tmp
-rm files/*.zip
+rm -rf .tmp > /dev/null 2>&1
+rm files/*.zip > /dev/null 2>&1
 
 echo "[+] Generating reports"
-
 cd files
 for PLUGIN in $(ls); do
 	cd $PLUGIN
